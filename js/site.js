8c48b870c937562decdd1e101270e9daa5ec0bfe
Danial Faizsal <m.d.faizsal@gmail.com>
2022-01-09 10:57:57 +0800

    subs to store, dispplaySubs to display, add to add, and sub properties updated

--- js/site.js
+++ js/site.js
@@ -1,3 +1,67 @@
-function HelloWorld {
+function HelloWorld() {
     alert("Hello World!");
+}
+
+var subs =  [
+        {subsName: "Netflix", 
+        subsUrl: "https://www.netflix.com/",
+        startDate: null,
+        endDate: "27 Jan 2022",
+        recurFee: 1748,
+        recurFeeCurrency: "SGD",
+        recurFeePeriod: "month"
+        }
+      ]
+
+displaySubs();
+
+function add(initialSubsName) {
+subs.push({
+  subsName: initialSubsName,
+  subsUrl: "https://www.exampleURL.com/",
+  startDate: null,
+  endDate: "27 Jan 2022",
+  recurFee: 1000,
+  recurFeeCurrency: "SGD",
+  recurFeePeriod: "month",
+});
+displaySubs();
+}
+
+function edit(position, newSubsName) {
+subs[position].subsName = newSubsName;
+displaySubs();
+}
+
+function remove(position) {
+subs.splice(position, 1);
+displaySubs();
+}
+
+/*
+function toggle(position) {
+if (subs[position].completed === true) {
+    subs[position].completed = false;
+} else {
+    subs[position].completed = true;
+}
+
+displaySubs();
+}
+*/
+
+function displaySubs() {
+for (i = 0; i < subs.length; i++) {
+    console.log(subs[i]);
+}
+        
+    function toggleAll() {
+        // if everything is false ==> all to true
+        // if mixed ==> all to true
+        /* if () {
+            
+        } else {}; */
+        // if everything is true ==> all to false
+        
+    }
 }
