be0de9c8c19a01f0d578def3a34878da7b262130
Danial Faizsal <m.d.faizsal@gmail.com>
2022-01-09 14:48:14 +0800

    added DOM events and adding new li items

--- js/site.js
+++ js/site.js
@@ -1,41 +1,86 @@
 function HelloWorld() {
-    alert("Hello World!");
+  alert("Hello World!");
 }
 
-var subs =  [
-        {subsName: "Netflix", 
-        subsUrl: "https://www.netflix.com/",
-        startDate: null,
-        endDate: "27 Jan 2022",
-        recurFee: 1748,
-        recurFeeCurrency: "SGD",
-        recurFeePeriod: "month"
-        }
-      ]
+var button = document.getElementById("enter");
+var input = document.getElementById("subInput");
+var ul = document.getElementById("sub-ul");
 
-displaySubs();
-
-function add(initialSubsName) {
-subs.push({
-  subsName: initialSubsName,
-  subsUrl: "https://www.exampleURL.com/",
-  startDate: null,
-  endDate: "27 Jan 2022",
-  recurFee: 1000,
-  recurFeeCurrency: "SGD",
-  recurFeePeriod: "month",
-});
-displaySubs();
+function inputLength() {
+    return input.value.length;
 }
 
-function edit(position, newSubsName) {
-subs[position].subsName = newSubsName;
+function createListElement() {
+    var li = document.createElement("li");
+    var btn = document.createElement("button");
+    btn.textContent = "Remove";
+    btn.id = "removeBtn";
+    li.appendChild(document.createTextNode(input.value));
+    li.appendChild(btn);
+    ul.appendChild(li);
+    input.value = "";
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement();
+    }
+}
+
+function addListAfterKeypress(event) {
+    if (inputLength() > 0 && event.keyCode === 13) {
+      createListElement();
+    }
+} 
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
+
+var subs = [
+  {
+    subsName: "Netflix",
+    subsUrl: "https://www.netflix.com/",
+    startDate: null,
+    endDate: "27 Jan 2022",
+    recurFee: 1748,
+    recurFeeCurrency: "SGD",
+    recurFeePeriod: "month",
+  },
+];
+
 displaySubs();
+
+
+
+function add(
+  initSubsName = "example sub",
+  initUrl = "https://www.exampleURL.com/",
+  initStartDate = null,
+  initEndDate = null,
+  initRecurFee = 0,
+  initRecurFeeCurrency = "SGD",
+  initRecurFeePeriod = "mth"
+) {
+  subs.push({
+    subsName: initSubsName,
+    subsUrl: initUrl,
+    startDate: initStartDate,
+    endDate: initEndDate,
+    recurFee: initRecurFee,
+    recurFeeCurrency: initRecurFeeCurrency,
+    recurFeePeriod: initRecurFeePeriod,
+  });
+  displaySubs();
+}
+
+function edit(position, subsProp, newInput) {
+  subs[position][subsProp] = newInput;
+  displaySubs();
 }
 
 function remove(position) {
-subs.splice(position, 1);
-displaySubs();
+  subs.splice(position, 1);
+  displaySubs();
 }
 
 /*
@@ -51,17 +96,7 @@ displaySubs();
 */
 
 function displaySubs() {
-for (i = 0; i < subs.length; i++) {
+  for (i = 0; i < subs.length; i++) {
     console.log(subs[i]);
-}
-        
-    function toggleAll() {
-        // if everything is false ==> all to true
-        // if mixed ==> all to true
-        /* if () {
-            
-        } else {}; */
-        // if everything is true ==> all to false
-        
-    }
+  }
 }
